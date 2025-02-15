8d5acda413ebdc5509a1dd08d4864fe6b272a5dd
rxiot <caleb.mojica@gmail.com>
2025-02-14 20:12:33 -0500

    added player movement

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ def main():
 
  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+ updatable = pygame.sprite.Group()
+ drawable = pygame.sprite.Group()
+ Player.containers = (updatable, drawable)
+
  print("Starting asteroids!", f"Screen width: {SCREEN_WIDTH}", f"Screen height: {SCREEN_HEIGHT}")
  clock = pygame.time.Clock()
  dt = 0
  x = SCREEN_WIDTH / 2
  y = SCREEN_HEIGHT / 2
- player = Player(x, y) 
+ player = Player(x, y)
  while True:
      for event in pygame.event.get():
        if event.type == pygame.QUIT:
-         return
+         return  
+       #limits framerate to 60 fps
      dt = clock.tick(60) / 1000
      screen.fill("black")
-     player.update(dt)
-     player.draw(screen)
+     updatable.update(dt)
+     for drawn in drawable:
+       drawn.draw(screen)
      pygame.display.flip()
 
 
