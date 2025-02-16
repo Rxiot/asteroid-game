cfec4177273266d916ad8e18bb982aa62e126183
rxiot <caleb.mojica@gmail.com>
2025-02-15 21:05:00 -0500

    created collision

--- asteroid.py
+++ asteroid.py
@@ -1,11 +1,11 @@
 from circleshape import *
-import pygame
+import pygame # type: ignore
 
 class Asteroid(CircleShape):
  def __init__(self, x, y, radius):
   super().__init__(x, y, radius)
   self.radius = radius
-  self.position = (x, y)
+  
 
  def draw(self, screen):
   pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import pygame # type: ignore
 from constants import *
-from player import *
-from asteroid import *
-from asteroidfield import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 def main():
  pygame.init()
 
  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
  
+ # INITALIZES OBJECTS
  updatable = pygame.sprite.Group()
  drawable = pygame.sprite.Group()
  Player.containers = (updatable, drawable)
  asteroids = pygame.sprite.Group()
  Asteroid.containers = (asteroids, updatable, drawable)
  AsteroidField.containers = (updatable)
+ 
  print("Starting asteroids!", f"Screen width: {SCREEN_WIDTH}", f"Screen height: {SCREEN_HEIGHT}")
+
  clock = pygame.time.Clock()
  dt = 0
  x = SCREEN_WIDTH / 2
  y = SCREEN_HEIGHT / 2
  player = Player(x, y)
- asteroidfield = AsteroidField()
+ asteroid_field = AsteroidField()
+
+
  while True:
      for event in pygame.event.get():
        if event.type == pygame.QUIT:
@@ -29,6 +35,10 @@ def main():
      dt = clock.tick(60) / 1000
      screen.fill("black")
      updatable.update(dt)
+     for asteroid in asteroids:
+       if asteroid.collision(player):
+         print("Game over!")
+         sys.exit()
      for drawn in drawable:
        drawn.draw(screen)
      pygame.display.flip()
