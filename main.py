130c0d8ab18d868a0d0ad5746dd41333e76d6064
rxiot <caleb.mojica@gmail.com>
2025-02-18 12:24:45 -0500

    added shooting ability

--- main.py
+++ main.py
@@ -3,36 +3,46 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 def main():
- pygame.init()
-
- screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
- 
+  pygame.init()
  # INITALIZES OBJECTS
- updatable = pygame.sprite.Group()
- drawable = pygame.sprite.Group()
- Player.containers = (updatable, drawable)
- asteroids = pygame.sprite.Group()
- Asteroid.containers = (asteroids, updatable, drawable)
- AsteroidField.containers = (updatable)
- 
- print("Starting asteroids!", f"Screen width: {SCREEN_WIDTH}", f"Screen height: {SCREEN_HEIGHT}")
 
- clock = pygame.time.Clock()
- dt = 0
- x = SCREEN_WIDTH / 2
- y = SCREEN_HEIGHT / 2
- player = Player(x, y)
- asteroid_field = AsteroidField()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ # GROUPS
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+  shot = pygame.sprite.Group()
+
+ # CONTAINERS
+  Player.containers = (updatable, drawable)
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = (updatable)
+  Shot.containers = (updatable, drawable, shot)
+
+ # ONLY SHOWN IN TERMINAL 
+  print("Starting asteroids!", f"Screen width: {SCREEN_WIDTH}", f"Screen height: {SCREEN_HEIGHT}")
+  #
+
+  clock = pygame.time.Clock()
+  dt = 0
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT / 2
+  player = Player(x, y)
+  asteroid_field = AsteroidField()
 
 
- while True:
+  while True:
      for event in pygame.event.get():
        if event.type == pygame.QUIT:
          return  
-       #limits framerate to 60 fps
+     
+     #limits framerate to 60 fps
      dt = clock.tick(60) / 1000
+     # sets background
      screen.fill("black")
      updatable.update(dt)
      for asteroid in asteroids:
--- player.py
+++ player.py
@@ -1,6 +1,9 @@
 from circleshape import CircleShape
 import pygame # type: ignore
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
+
+
 class Player(CircleShape):
  def __init__(self, x, y):
   super().__init__(x, y, PLAYER_RADIUS)
@@ -24,8 +27,9 @@ class Player(CircleShape):
    self.move(dt)
   if keys[pygame.K_s]:
    self.move(-dt)
+  if keys[pygame.K_SPACE]:
+   self.shoot()
 
-   
 
  def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,4 +40,8 @@ class Player(CircleShape):
     return [a, b, c]
  
  def draw(self, screen):
-  pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+  pygame.draw.polygon(screen, "yellow", self.triangle(), width=2)
+
+ def shoot(self):
+  shot = Shot(self.position.x, self.position.y)
+  shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
